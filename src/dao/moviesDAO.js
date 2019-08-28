c63d0afc54c15b39bb171cf2567ceea26430ed2e
ying <ying@yings-MacBook-Pro.local>
2019-08-28 14:31:46 -0400

    finish resiliency setting - connection pooling, timeouts, handling errors

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -342,8 +342,8 @@ export default class MoviesDAO {
 
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
-      console.error(`Something went wrong in getMovieByID: ${e}`)
-      throw e
+      console.error(`Something went wrong in getMovieByID: ${e.message}`)
+      return null;
     }
   }
 }
